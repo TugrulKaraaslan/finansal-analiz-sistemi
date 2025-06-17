0b9e0a541ce9dbb2161faab22cb91a7aba29a1dd
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-06-17 23:40:11 +0300

    Add simple Streamlit report viewer

---
+++ gui/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Rapor Görüntüleyici")
+
+uploaded = st.file_uploader("Rapor Excel’i (.xlsx) seç", type="xlsx")
+if uploaded:
+    with st.spinner("Yükleniyor..."):
+        xls = pd.ExcelFile(uploaded)
+        tab1, tab2 = st.tabs(["Özet", "Hatalar"])
+        with tab1:
+            st.dataframe(pd.read_excel(xls, "Özet"))
+        with tab2:
+            hatalar = pd.read_excel(xls, "Hatalar")
+            st.dataframe(hatalar.style.apply(
+                lambda s: ["background-color:#faa" if v!="OK" else "" for v in s["durum"]], axis=1))
