1d8981ce0023ae55c5dd4c24e882631a1be753e7
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-06-17 20:46:59 +0300

    Add PSAR vs close crossover outputs

--- config.py
+++ config.py
@@ -470,7 +470,23 @@ SERIES_SERIES_CROSSOVERS = [
         "ichimoku_conversionline_keser_close_yukari",
         "ichimoku_conversionline_keser_close_asagi",
     ),
+    (
+        "psar",
+        "close",
+        "psar_keser_close_yukari",
+        "psar_keser_close_asagi",
+    ),
 ]
+# --- crossover çıktıları da her zaman hesaplansın
+try:
+    wanted_cols
+except NameError:
+    wanted_cols = set()
+wanted_cols |= {
+    c_above for _, _, c_above, _ in SERIES_SERIES_CROSSOVERS
+} | {
+    c_below for _, _, _, c_below in SERIES_SERIES_CROSSOVERS
+}
 
 SERIES_VALUE_CROSSOVERS = [
     ("rsi_14", 50.0, "50p0"),
