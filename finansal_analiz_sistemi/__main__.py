e23f2977a93a44d63b7e3d7e9893fe2b2af54701
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-06-23 17:31:35 +0300

    Revert "feat(cli): unify package entry point"

--- finansal_analiz_sistemi/__main__.py
+++ finansal_analiz_sistemi/__main__.py
@@ -1,5 +1,3 @@
-"""`python -m finansal_analiz_sistemi` shortcut."""
 import runpy
 
-runpy.run_module("finansal_analiz_sistemi.main", run_name="__main__")
-
+runpy.run_module("run", run_name="__main__")
