56185b84994bd45e65bd54108dce0790233a8dbe
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-06-28 15:44:24 +0300

    fix: expose excel_reader module

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,6 +4,12 @@ Exports wrappers around :mod:`openpyxl`'s ``ExcelFile`` handling
 implemented in :mod:`.excel_reader`.
 """
 
+from . import excel_reader
 from .excel_reader import clear_cache, open_excel_cached, read_excel_cached
 
-__all__ = ["open_excel_cached", "read_excel_cached", "clear_cache"]
+__all__ = [
+    "excel_reader",
+    "open_excel_cached",
+    "read_excel_cached",
+    "clear_cache",
+]
