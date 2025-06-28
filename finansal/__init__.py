b095d788490be0c889f95623238443aef7c089fc
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-06-28 12:39:48 +0300

    fix: expose helper APIs via finansal package

--- finansal/__init__.py
+++ finansal/__init__.py
@@ -1,1 +1,9 @@
-"""Parquet cache management package."""
+"""Convenience exports for :mod:`finansal` package."""
+
+from importlib import import_module
+
+ParquetCacheManager = import_module("finansal.parquet_cache").ParquetCacheManager
+lazy_chunk = import_module("finansal.utils").lazy_chunk
+safe_set = import_module("finansal.utils").safe_set
+
+__all__ = ["ParquetCacheManager", "lazy_chunk", "safe_set"]
