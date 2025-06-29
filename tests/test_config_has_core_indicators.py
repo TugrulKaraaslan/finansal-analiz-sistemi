f2bfc90f19ab21e8394b644be186a70d8d3f1c41
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-06-30 02:35:44 +0300

    fix: address flake8 in CORE_INDICATORS test

--- tests/test_config_has_core_indicators.py
+++ tests/test_config_has_core_indicators.py
@@ -1,9 +1,8 @@
+import importlib
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-import importlib
 
 
 def test_core_indicators_defined():
