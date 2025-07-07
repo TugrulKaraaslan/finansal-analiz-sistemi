768eabc8394438a760dacc3bf6f20bfab41c6634
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-07-07 04:35:01 +0300

    Update OpenBB integration

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,5 +1,7 @@
+import importlib.metadata
 import importlib.util
 
 
-def test_pandas_ta_installed():
+def test_pandas_ta_openbb_installed():
     assert importlib.util.find_spec("pandas_ta") is not None
+    assert importlib.metadata.version("pandas-ta-openbb")
