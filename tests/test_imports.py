e8d7487f95d7ae991c739289495d84e6951cdfd5
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-07-10 16:21:23 +0300

    chore: remove duplicate import test

--- tests/test_imports.py
+++
@@ -1,8 +0,0 @@
-"""Unit tests for imports."""
-
-import importlib
-
-
-def test_pandas_ta_available():
-    """The optional ``pandas_ta`` package should be importable."""
-    assert importlib.util.find_spec("pandas_ta") is not None
