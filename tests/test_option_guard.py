e2301788acfc0cbecfe242c43d98c0774bde464b
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-07-19 00:22:04 +0300

    pandas opsiyon yardimcisini iyilestir ve test ekle

--- tests/test_option_guard.py
+++ tests/test_option_guard.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-from utils.pandas_option_safe import ensure_option
+from utils.pandas_option_safe import ensure_option, option_context
 
 
 def test_ensure_option_no_error():
@@ -13,3 +13,18 @@ def test_ensure_option_no_error():
     except (AttributeError, KeyError, pd.errors.OptionError):
         return
     assert val is True
+
+
+def test_option_context_valid_resets():
+    """Değişken bir pandas seçeneği doğru şekilde geçici olarak ayarlanmalı."""
+    opt = "display.expand_frame_repr"
+    original = pd.get_option(opt)
+    with option_context(opt, not original):
+        assert pd.get_option(opt) is (not original)
+    assert pd.get_option(opt) is original
+
+
+def test_option_context_ignores_invalid():
+    """Olmayan bir seçenek hataya yol açmamalı."""
+    with option_context("does.not.exist", True):
+        assert True
