972cf6671dbc5a690af034354e518adc7974ff1b
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-08-04 02:00:15 +0300

    Remove test suite

--- tests/test_key_columns.py
+++
@@ -1,6 +0,0 @@
-"""Unit tests for key_columns."""
-
-
-def test_required_columns_present(summary_df):
-    """Summary DataFrame must include key columns used in merges."""
-    assert {"filtre_kodu", "hisse_kodu"} <= set(summary_df.columns)
