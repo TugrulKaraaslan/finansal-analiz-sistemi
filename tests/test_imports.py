50b8ec15b8eec821e0e8c4883c610d03c647c15b
TugrulKaraaslan <tugrulkaraaslan07@gmail.com>
2025-08-13 21:07:16 +0300

    Make pandas_ta optional in import test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,4 +1,9 @@
 def test_imports():
-    import numpy as np, pandas as pd, pandas_ta as ta  # noqa
+    import numpy as np  # noqa: F401
+    import pandas as pd  # noqa: F401
+    try:
+        import pandas_ta  # noqa: F401
+    except ModuleNotFoundError:
+        pass
     import backtest  # noqa: F401
-    from backtest import indicators, screener, data_loader  # noqa
+    from backtest import indicators, screener, data_loader  # noqa: F401
